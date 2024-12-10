b4c95b422f2a614ba5dc663d333244d6f8f9717d
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-12-10 17:33:58 +0000

    aula dia 10-12

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -3,32 +3,32 @@ import Menu from "./components/navbar";
 import Footer from "./components/footer";
 import CardProd from "./cardprod";
 
-export default function Pesportes({produtos}) {
-    return <>
-        <Menu />
-        <Container className="border border-2 
+export default function Pesportes({ produtos }) {
+  return <>
+    <Menu />
+    <Container className="border border-2 
     rounded border-primary col-sm-6 text-center">
-            Lista de Produtos de Esportes
-        </Container>
-        <Container>
-          <div >
-          <h1>Produtos Esportes</h1>
-          <div className="row row-cols-1 row-cols-md-5 g-3">
-            {produtos.map(produto=>
+      Lista de Produtos de Esportes
+    </Container>
+    <Container>
+      <div >
+        <h1>Produtos Esportes</h1>
+        <div className="row row-cols-1 row-cols-md-5 g-3">
+          {produtos.map(produto =>
             <div key={produto.id}>
-                  <CardProd titulo={produto.titulo}
-                  descricao={produto.descricao}
-                  preco={produto.preco}/>
-                  </div>
-                )}
-          </div>
-          </div>
-        </Container>
-        <Footer />
-    </>
+              <CardProd titulo={produto.titulo}
+                descricao={produto.descricao}
+                preco={produto.preco} />
+            </div>
+          )}
+        </div>
+      </div>
+    </Container>
+    <Footer />
+  </>
 }
 export async function getServerSideProps() {
-    const res = await fetch('http://localhost:3000/api/produtos/data')
-    const repo = await res.json()
-    return { props: { produtos: repo } }
+  const res = await fetch('http://localhost:3000/api/produtos/data')
+  const repo = await res.json()
+  return { props: { produtos: repo } }
 }
